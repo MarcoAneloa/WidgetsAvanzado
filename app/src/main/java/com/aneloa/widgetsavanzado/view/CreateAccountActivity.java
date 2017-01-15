4c7be4594bf5b1ab4e7a1e339d6d0aa57a97db08
MarcoAneloa <maneloa89@gmail.com>
2017-01-15 00:53:21 -0500

    Initial commit

---
+++ app/src/main/java/com/aneloa/widgetsavanzado/view/CreateAccountActivity.java
@@ -0,0 +1,15 @@
+package com.aneloa.widgetsavanzado.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.aneloa.widgetsavanzado.R;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+    }
+}
