37128b1bfe4b433d5e64e8eeb9a2452c547c9b31
MarcoAneloa <maneloa89@gmail.com>
2017-01-31 00:10:58 -0500

    fragments

---
+++ app/src/main/java/com/aneloa/widgetsavanzado/view/ConteinerActivity.java
@@ -0,0 +1,15 @@
+package com.aneloa.widgetsavanzado.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.aneloa.widgetsavanzado.R;
+
+public class ConteinerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_conteiner);
+    }
+}
