4da6b1150a4a5ce9dbf228cd345f7abbce488a9c
MarcoAneloa <maneloa89@gmail.com>
2017-04-14 23:12:46 -0500

    fragments

--- app/src/main/java/com/aneloa/widgetsavanzado/view/fragments/HomeFragment.java
+++ app/src/main/java/com/aneloa/widgetsavanzado/view/fragments/HomeFragment.java
@@ -3,11 +3,19 @@ package com.aneloa.widgetsavanzado.view.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.aneloa.widgetsavanzado.R;
+import com.aneloa.widgetsavanzado.adapter.PictureAdapterRecyclerView;
+import com.aneloa.widgetsavanzado.model.Picture;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -24,7 +32,32 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view=inflater.inflate(R.layout.fragment_home, container, false);
+        showToolbar("Home",false,view);
+        RecyclerView picturesRecycler= (RecyclerView) view.findViewById(R.id.pictureRecycler);
+
+        LinearLayoutManager linearLayoutManager= new LinearLayoutManager(getContext());
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+
+        picturesRecycler.setLayoutManager(linearLayoutManager);
+
+        PictureAdapterRecyclerView pictureAdapterRecyclerView=new PictureAdapterRecyclerView(buidPictures(),R.layout.cardview_picture,getActivity());
+        picturesRecycler.setAdapter(pictureAdapterRecyclerView);
+        return view;
     }
 
+    public ArrayList<Picture> buidPictures(){
+        ArrayList<Picture> pictures= new ArrayList<>();
+        pictures.add(new Picture("http://www.novalandtours.com/images/guide/guilin.jpg","Marco Aneloa","4 dias","3"));
+        pictures.add(new Picture("https://www.google.com.ec/search?q=imagenes+de+naruto&espv=2&tbm=isch&tbo=u&source=univ&sa=X&ved=0ahUKEwjfpdary6XTAhVEbiYKHaiBC88QsAQIIA&biw=1366&bih=613&dpr=1#imgrc=_fkU-GQjmB8UPM:","Juan Pablo","3 dias","10"));
+        pictures.add(new Picture("http://www.novalandtours.com/images/guide/guilin.jpg","Ximena Mangia","4 dias","15"));
+        return pictures;
+    }
+
+    public void showToolbar(String tittle,boolean upButton, View view){
+        Toolbar toolbar=(Toolbar) view.findViewById(R.id.toolbar);
+        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle(tittle);
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(upButton);
+    }
 }
